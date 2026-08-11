f99b6bec73f2cf440530b684535cc98fb2b68508
rajaLucy <rajakiranyadavalli@gmail.com>
2026-08-11 15:46:50 +0530

    RDKEMW-20294: Fix all native build errors

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -63,6 +63,12 @@ grep -q "virtual ~IAudioOutputPortEvents()" entservices-testframework/Tests/mock
     sed -i 's/class IAudioOutputPortEvents {/class IAudioOutputPortEvents {\n    public:\n        virtual ~IAudioOutputPortEvents() = default;/' \
         entservices-testframework/Tests/mocks/devicesettings.h
 
+# Patch ServiceMock.h: testframework 1.0.14 declares AddRef() returning void,
+# but Thunder R4_4-RDK changed IReferenceCounted::AddRef() to return uint32_t.
+grep -q "MOCK_METHOD(uint32_t, AddRef" entservices-testframework/Tests/mocks/thunder/ServiceMock.h || \
+    sed -i 's/MOCK_METHOD(void, AddRef/MOCK_METHOD(uint32_t, AddRef/' \
+        entservices-testframework/Tests/mocks/thunder/ServiceMock.h
+
 ############################
 # Build Thunder-Tools
 echo "====================================================================================="
