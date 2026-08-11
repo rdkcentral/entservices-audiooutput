23c238f4de91364a33d541fce2f05c9034a87730
RajaLucy <133349260+RajaLucy@users.noreply.github.com>
2026-08-11 12:01:15 +0530

    RDKEMW-20294:Update build_dependencies.sh

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -45,9 +45,9 @@ cd ..
 # Clone the required repositories
 
 
-git clone --branch  R4.4.3 https://github.com/rdkcentral/ThunderTools.git
+git clone --branch  R4_4-RDK https://github.com/rdkcentral/ThunderTools.git
 
-git clone --branch R4.4.1 https://github.com/rdkcentral/Thunder.git
+git clone --branch R4_4-RDK https://github.com/rdkcentral/Thunder.git
 
 git clone --branch 4.1.2 https://github.com/rdkcentral/entservices-apis.git
 
