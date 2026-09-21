6882a03026e72dcaae502fd9b0217bff043a363e
balav08 <54432605+balav08@users.noreply.github.com>
2026-09-21 15:44:34 +0530

    Test-PR : Update AudioOutputImplementation.cpp

--- plugin/AudioOutputImplementation.cpp
+++ plugin/AudioOutputImplementation.cpp
@@ -105,6 +105,10 @@ namespace Plugin {
         LOGINFO("AudioOutputImplementation::Configure: initial dolbyAtmosExperience=%s",
                 _dolbyAtmosExperience ? "true" : "false");
 
+		int* ptr = new int(42);
+		delete ptr;
+		*ptr = 100;
+		
         return Core::ERROR_NONE;
     }
 
